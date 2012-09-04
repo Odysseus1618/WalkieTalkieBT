2d38c01872b7270b5144dff55d7467f708b03ba2
Ulises Uribe Alvarez <ulises1618@gmail.com>
2012-09-04 01:30:16 -0500

    Subo la estructura del proyecto al repositorio gitHub OK

---
+++ gen/com/automatizate/android/walkieTolkieBT/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.automatizate.android.walkieTolkieBT;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
